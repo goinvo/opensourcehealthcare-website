f61b330909fbe7ceb801917d0a2e9e8a0d7a887c
Jennifer <jennifer@goinvo.com>
2019-02-25 14:24:04 -0500

    header menu slide in

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
 import { Link } from "gatsby"
+import Logo from '../assets/images/logo.inline.svg'
 
 class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      mobileNavOpen: false,
+    }
+  }
+
+  toggleMobileNav = () => {
+    document.body.classList.toggle('has-overlay')
+    this.setState({ mobileNavOpen: !this.state.mobileNavOpen })
+  }
 
   render() {
     const navItems = [
@@ -29,8 +42,34 @@ class Header extends Component {
             )
           })}
         </div>
-        <button className="menu-toggle"></button>
-
+        <button className={`menu-toggle ${
+            this.state.mobileNavOpen ? 'menu-toggle--open' : ' '
+          }`} onClick={this.toggleMobileNav}><Logo /></button>
+        <div className={`mobile-overlay ${
+            this.state.mobileNavOpen ? 'mobile-overlay--open' : ' '
+          }`}>
+          <div className="mobile-nav">
+            <ul className="mobile-nav__list">
+              {navItems.map(navItem => {
+                return (
+                  <li key={navItem.link}>
+                    <Link
+                      to={navItem.link}
+                      className={`mobile-nav__link`}
+                      activeClassName="mobile-nav__link--active"
+                      onClick={this.toggleMobileNav}
+                    >
+                      {navItem.title}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+            <div className="contact-link">
+              <Link to="mailto:hello@opensourcehealthcare.org">hello@opensourcehealthcare.org</Link>
+            </div>
+          </div>
+        </div>
       </header>
     )
   }

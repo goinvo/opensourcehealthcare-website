b0eccd2af91f03613c338bdbde9eea79acee0ada
Jennifer <jennifer@goinvo.com>
2019-02-25 16:32:16 -0500

    home styling, footer phone number, minor style changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,12 +7,12 @@ const Footer = ({ children }) => (
       <p className="text--center">Who are we?</p>
       <h3 className="text--center margin-bottom--none">We are GoInvo</h3>
       <p className="text--center margin-top--none">GoInvo is a design studio focused on creating an open source future to drive better healthcare for all.</p>
-      <p className="text--center"><Link to="https://www.goinvo.com/work/?category=open-source">See our company's open source projects</Link></p>
+      <p className="text--center"><Link to="https://www.goinvo.com/work/?category=open-source">Our open source projects</Link></p>
       <p className="text--center">
         <Link to="mailto:hello@opensourcehealthcare.org">hello@opensourcehealthcare.org</Link>
         <br />661 Massachusetts Ave, 3rd Floor,
         <br />Arlington, MA 02476
-        <br /><a href="tel:+16178037043">617-803-7043</a>
+        <br /><a href="tel:+16175043390">617.504.3390</a>
       </p>
     </div>
   </footer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,26 @@ import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-    <div className="max-width content-padding pad-vertical pad-top--double header-padding">
-      <div class="top">
-        <h1>We demand open source healthcare</h1>
-        <h1>We must set healthcare free</h1>
-      </div>
 
-      <div class="mission">
-        <h4>Open Source (OS) Software is</h4>
-        <p></p>
+      <div className="background--dark-gradient">
+        <div className="max-width content-padding pad-vertical pad-top--double header-padding">
+        <div class="top">
+          <h1 className="text--center mobile-squish">We demand <span className="text--red">open source</span> healthcare</h1>
+          <h2 className="text--center mobile-squish header-xl">We must set healthcare free</h2>
+          <Link to="/page-2/">Check out the Open Source Healthcare journal</Link>
+        </div>
+
+        <div class="mission">
+          <h4>Open Source (OS) Software is</h4>
+          <p>something that anyone can change and share, because it’s publicly available under a generous license. While it first began with computer code, open source now influences how projects and businesses work, and our lives benefit from this open sharing. Open source has grown into a way of participating with many others that asks for transparency, community-based collaboration, and meritocracy. The best ideas float to the top, and you earn trust by what you do and how you amplify the group.</p>
+          <p>Our internet is infused with open source ideas and services &mdash; from how cell phones communicate, to how e-mail is directed from one person to the next, to Linux. All of these technologies working together are the operating system of the internet.</p>
+          <p>Here in the US, healthcare is
+            <br />sometimes amazing,
+            <br />often lifesaving,
+            <br />always expensive,
+            <br />and mostly closed.</p>
+          <p>It's tribal at its core &mdash; each hospital, each doc, each healthcare system invents its own way &mdash;to the detriment of our collective health.</p>
+        </div>
       </div>
 
       <Link to="/page-2/">Go to page 2</Link>

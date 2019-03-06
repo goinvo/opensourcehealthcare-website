7179cdd8af4674dc9e3f25bcf849b9d68e09b5c1
Jennifer Patel <jennifer@goinvo.com>
2019-03-06 10:23:49 -0500

    header & index

--- src/components/header.js
+++ src/components/header.js
@@ -28,45 +28,48 @@ class Header extends Component {
 
     return (
       <header>
-        <div className="header-nav">
-          {navItems.map(navItem => {
-            return (
-              <Link
-                key={navItem.link}
-                to={navItem.link}
-                className={`header-nav__link ${navItem.class}`}
-                activeClassName="header-nav__link--active"
-              >
-                {navItem.title}
-              </Link>
-            )
-          })}
-        </div>
-        <button className={`menu-toggle ${
-            this.state.mobileNavOpen ? 'menu-toggle--open' : ' '
-          }`} onClick={this.toggleMobileNav}><Logo /></button>
-        <div className={`mobile-overlay ${
-            this.state.mobileNavOpen ? 'mobile-overlay--open' : ' '
-          }`}>
-          <div className="mobile-nav">
-            <ul className="mobile-nav__list">
-              {navItems.map(navItem => {
-                return (
-                  <li key={navItem.link}>
-                    <Link
-                      to={navItem.link}
-                      className={`mobile-nav__link`}
-                      activeClassName="mobile-nav__link--active"
-                      onClick={this.toggleMobileNav}
-                    >
-                      {navItem.title}
-                    </Link>
-                  </li>
-                )
-              })}
-            </ul>
-            <div className="contact-link">
-              <Link to="mailto:hello@opensourcehealthcare.org">hello@opensourcehealthcare.org</Link>
+        <div class="header-wrapper">
+          <div className="header-nav">
+            {navItems.map(navItem => {
+              return (
+                <Link
+                  key={navItem.link}
+                  to={navItem.link}
+                  className={`header-nav__link ${navItem.class}`}
+                  activeClassName="header-nav__link--active"
+                >
+                  {navItem.title}
+                </Link>
+              )
+            })}
+          </div>
+          <Link to="/" className="menu-toggle hidden--until-lg"><Logo /></Link>
+          <button className={`menu-toggle hidden--lg ${
+              this.state.mobileNavOpen ? 'menu-toggle--open' : ' '
+            }`} onClick={this.toggleMobileNav}><Logo /></button>
+          <div className={`mobile-overlay ${
+              this.state.mobileNavOpen ? 'mobile-overlay--open' : ' '
+            }`}>
+            <div className="mobile-nav">
+              <ul className="mobile-nav__list">
+                {navItems.map(navItem => {
+                  return (
+                    <li key={navItem.link}>
+                      <Link
+                        to={navItem.link}
+                        className={`mobile-nav__link`}
+                        activeClassName="mobile-nav__link--active"
+                        onClick={this.toggleMobileNav}
+                      >
+                        {navItem.title}
+                      </Link>
+                    </li>
+                  )
+                })}
+              </ul>
+              <div className="contact-link">
+                <Link to="mailto:hello@opensourcehealthcare.org">hello@opensourcehealthcare.org</Link>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
+import ArticleCard from '../components/articleCard'
+import spotlightItems from '../data/article-spotlight-order'
 
 const IndexPage = () => (
   <Layout>
     <div className="hero-container hero-container--home">
       <div className="hero hero--fixed hero--home">
         <div className="max-width content-padding pad-vertical header-padding">
-          <h1 className="text--center mobile-squish-sm">We demand <span className="text--red">open source</span> healthcare</h1>
-          <h2 className="text--center mobile-squish-sm header-xl">We must set healthcare free</h2>
+          <div className="header-container">
+            <h1 className="text--center mobile-squish-sm">We demand <span className="text--red"> open source </span> healthcare</h1>
+            <h2 className="text--center mobile-squish-sm header-xl">We must set healthcare free</h2>
+          </div>
           <p className="text--center"><Link to="/downloads/open-source-healthcare-journal.pdf" target="_blank" rel="noopener">Check out the Open Source Healthcare journal</Link></p>
         </div>
       </div>
@@ -30,7 +34,74 @@ const IndexPage = () => (
         </div>
       </div>
     </div>
+
+    <div className="max-width content-padding pad-vertical">
+      <div className="reasons">
+        <div className="reason">
+          <p>We have open standards for finance,</p>
+          <p>because we value our money more than our health.</p>
+        </div>
+        <div className="reason">
+          <p>We have open standards for finance</p>
+          <p>because getting to the destination is a necessity.</p>
+        </div>
+        <div className="reason">
+          <p>We have open standards for finance</p>
+          <p>because <strong>our lives depend on it</strong>.</p>
+        </div>
+      </div>
+    </div>
+
+    <div className="max-width content-padding pad-vertical">
+      <h3>Healthcare is too important to be closed.</h3>
+      <p>Our healthcare services need to be open. We are working towards an open source future to drive better healthcare for all.</p>
+      <div class="button-container"><Link to="/mission/" className="button button--primary">See our mission</Link></div>
+    </div>
+
+    <div className="max-width content-padding pad-vertical">
+      <div className="article-section">
+        <h2 className="header-xl">Spotlight</h2>
+        {spotlightItems.map((spotlightArticle, i) => {
+          return (
+            <ArticleCard
+              link={spotlightArticle.link}
+              author={spotlightArticle.author}
+              title={spotlightArticle.title}>
+            </ArticleCard>
+          )
+        })}
+      </div>
+    </div>
+
+    <div className="max-width content-padding pad-vertical">
+      <div className="editors">
+        <h3>Editors</h3>
+        <h3 class="text--red">Open EHRs Are Going Global</h3>
+        <p>Creating,</p>
+      </div>
+    </div>
+
   </Layout>
 )
+
+export const indexPageQuery = graphql`
+  query IndexQuery {
+    allMdx {
+      edges {
+        node {
+          parent {
+            ... on File {
+              name
+            }
+          }
+          frontmatter {
+            author
+            title
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage

7729667d913da9b708e051ea57f1624ca7b32f80
Jennifer Patel <jennifer@goinvo.com>
2019-03-13 11:44:29 -0400

    static pdf links for download page

--- src/pages/downloads.js
+++ src/pages/downloads.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { Link, withPrefix } from 'gatsby'
 
 import Layout from '../components/layout'
-import DownloadCard from '../components/downloadCard'
-import downloadItems from '../data/downloads-order.json'
+import MyImage from '../components/myImage'
+import Columns from '../components/Columns'
+
+import pdf from 'downloads/open-source-healthcare-journal.pdf'
 
 class DownloadPage extends Component {
 
@@ -18,20 +19,35 @@ class DownloadPage extends Component {
             <br/>Share your OS points of view to public.</p>
 
           <div className="downloads-section">
-            {downloadItems.map((download, i) => {
-              return (
-                <DownloadCard
-                  title={download.title}
-                  image={download.image}
-                  link={download.link}
-                  linkText={download.linkText} >
-                </DownloadCard>
-              )
-            })}
-          </div>
-          <Link to={withPrefix('downloads/open-source-healthcare-journal.pdf')} className="button button--primary" target="_blank" rel="noopener noreferrer">Download 25MB PDF</Link>
-          <Link to='http://www.blurb.com/b/8980724-open-source-healthcare-journal' className="button" target="_blank" rel="noopener noreferrer">Buy $12 Blurb book</Link>
+            <Columns columns={3}>
+              <div className="download-card-container">
+                <div className="download-card">
+                  <div className="title">Open Source Healthcare</div>
+                  <MyImage filename="oshc-journal-home.png" />
+                </div>
+                <a href={pdf} className="button" target="_blank" rel="noopener noreferrer">Download 25MB PDF</a>
+                <a href="http://www.blurb.com/b/8980724-open-source-healthcare-journal" className="button" target="_blank" rel="noopener noreferrer">$12 Blurb magazine</a>
+              </div>
 
+              <div className="download-card-container">
+                <div className="download-card">
+                  <div className="title"><span className="text--red">Nine Organizing Principles</span> of Open Source Healthcare</div>
+                  <MyImage filename="oshc-journal-home.png" />
+                </div>
+                <a href="" className="button" target="_blank" rel="noopener noreferrer">Download PDF</a>
+              </div>
+
+              <div className="download-card-container">
+                <div className="download-card">
+                  <div className="title">Open Source Healthcare Missionette</div>
+                  <MyImage filename="oshc-journal-home.png" />
+                </div>
+                <a href="" className="button" target="_blank" rel="noopener noreferrer">Download PDF</a>
+              </div>
+
+            </Columns>
+
+          </div>
         </div>
       </Layout>
     )}

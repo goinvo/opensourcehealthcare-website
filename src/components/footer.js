9ecbf8cda8ee0585580758e0716f5ccdbb67f6ea
Jennifer Patel <jennifer@goinvo.com>
2019-03-14 11:28:06 -0400

    footer desktop layout

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,17 +3,21 @@ import { Link } from 'gatsby'
 
 const Footer = ({ children }) => (
   <footer>
-    <div className="max-width content-padding pad-vertical pad-top--double">
-      <p className="text--center">Who are we?</p>
-      <h3 className="text--center margin-bottom--none">We are GoInvo</h3>
-      <p className="text--center margin-top--none">GoInvo is a design studio focused on creating an open source future to drive better healthcare for all.</p>
-      <p className="text--center"><Link to="https://www.goinvo.com/work/?category=open-source">Our open source projects</Link></p>
-      <p className="text--center">
-        <a href="mailto:hello@opensourcehealthcare.org">hello@opensourcehealthcare.org</a>
-        <br />661 Massachusetts Ave, 3rd Floor,
-        <br />Arlington, MA 02476
-        <br /><a href="tel:+16175043390">617.504.3390</a>
-      </p>
+    <div className="max-width content-padding pad-vertical">
+      <div className="top-left">
+        <p>Who are we?</p>
+        <h3 className="margin-bottom--none">We are GoInvo</h3>
+        <p className="margin-top--none">GoInvo is a design studio focused on creating an open source future to drive better healthcare for all.</p>
+        <p><Link to="https://www.goinvo.com/work/?category=open-source">Our open source projects</Link></p>
+      </div>
+      <div className="bottom-right">
+        <p>
+          <a href="mailto:hello@opensourcehealthcare.org">hello@opensourcehealthcare.org</a>
+          <br />661 Massachusetts Ave, 3rd Floor,
+          <br />Arlington, MA 02476
+          <br /><a href="tel:+16175043390">617.504.3390</a>
+        </p>
+      </div>
     </div>
   </footer>
 )
